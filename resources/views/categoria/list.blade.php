8f922720359c81b3d2faf1480752da5d359d300a
Ihago <ihagog12@gmail.com>
2020-03-23 00:02:14 -0300

    lista categorias

--- resources/views/categoria/list.blade.php
+++ resources/views/categoria/list.blade.php
@@ -3,32 +3,29 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h3>Categoria</h3>
+            <h3>Categorias</h3>
         </div>
-        <div class="col-md-12">
-            <div class="container">
-                <div class="row justify-content-left">
-                    @foreach($modalidades as $modalidade)
-                        <div class="styleCategoria_button_padding">
-                            <a href="{{ route('categoria.show', ['id' => $modalidade->id]) }}"class="btn styleCategoria_button" style="display: block; line-height: 110px;">
-                                <span style="line-height: normal; display: inline-block; vertical-align: middle;">
-                                <div class="container" >
-                                    <div class="row justify-content-center">
-                                        <div class="col-sm-12 styleCategoria_imagem">
-                                            <img src="{{ asset('/icones/' . $modalidade->icone ) }}" alt="{{ $modalidade->nome }}" width="35px;">
-                                        </div>
-                                        <div class="col-sm-12">
-                                            <label>{{ $modalidade->nome }}</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                </span>
-                            </a>
+    </div>
+        
+    <div class="row justify-content-center">
+        @foreach($modalidades as $modalidade)
+            <div class="styleCategoria_button_padding">
+                <a href="{{ route('categoria.show', ['id' => $modalidade->id]) }}"class="btn styleCategoria_button" style="display: block; line-height: 110px;">
+                    <span style="line-height: normal; display: inline-block; vertical-align: middle;">
+                    <div class="container" >
+                        <div class="row justify-content-center">
+                            <div class="col-sm-12 styleCategoria_imagem">
+                                <img src="{{ asset('/icones/' . $modalidade->icone ) }}" alt="{{ $modalidade->nome }}" width="35px;">
+                            </div>
+                            <div class="col-sm-12">
+                                <label>{{ $modalidade->nome }}</label>
+                            </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                    </span>
+                </a>
             </div>
-        </div>
+        @endforeach
     </div>
 
 
