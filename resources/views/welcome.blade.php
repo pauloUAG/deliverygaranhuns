fa8fca58e16c3b3190e9a1335f473e87cbb5e27e
Danillo Bion <danilobion@gmail.com>
2020-04-21 18:34:32 -0300

    corpo da pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,40 +19,60 @@
 
     {{-- Destaques --}}
     <div class="row layout_index">
-        <div class="col-sm-12" style="margin-bottom: -100px">
+        <div class="col-sm-12" style="margin-bottom: -100px; margin-top:-3.5rem;">
             <h1 class="title">Destaques</h1>
         </div>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-md-12" style="padding-top: 2rem;padding-bottom: 2rem;">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="background-color: gray;">
-                <div class="carousel-inner">
-                    @if(count($imagem) > 0)
-                        <div class="carousel-item active">
-                            <img src="{{asset('storage/imagens/' . $imagem[0]->imagem)}}" class="d-block w-100"  alt="Primeiro Slide">
-                        </div>
-                        @for($i = 1 ; $i < count($imagem) ; $i++)
-                            @if(isset($imagem[$i]->imagem) && $imagem[$i]->imagem!="")
-                                <div class="carousel-item">
-                                    <img src="{{asset('storage/imagens/' . $imagem[$i]->imagem)}}" class="d-block w-100"  alt="Primeiro Slide">
-                                </div>
-                            @endif
-                        @endfor
-                    @else
-                        <div class="carousel-item active">
-                            <img src="{{asset('slides/aviso_5.jpg')}}" class="d-block w-100" alt="Terceiro Slide" >
-                        </div>
-                    @endif
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="left: -60px">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style="right: -60px">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Próximo</span>
-                </a>
+    <div class="row justify-content-center" style="margin-top:-1.5rem;">
+        <div class="col-md-11">
+          <div class="row justify-content-center">
+
+            <!-- Botao voltar -->
+            <div class="col">
+              <img class="carousel-control-prev" id="carrousel_botao_voltar" href="#carouselExampleControls" role="button" data-slide="prev" src="{{asset('icones/voltar_azul_icone.svg')}}" alt="">
+              <!-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="left: -60px">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Anterior</span>
+              </a> -->
             </div>
+            <!--x Botao voltar x-->
+
+            <!-- Slideshow -->
+            <div class="col-sm-11">
+                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="background-color: gray;">
+                     <div class="carousel-inner">
+                         @if(count($imagem) > 0)
+                             <div class="carousel-item active">
+                                 <img src="{{asset('storage/imagens/' . $imagem[0]->imagem)}}" class="d-block w-100"  alt="Primeiro Slide">
+                             </div>
+                             @for($i = 1 ; $i < count($imagem) ; $i++)
+                                 @if(isset($imagem[$i]->imagem) && $imagem[$i]->imagem!="")
+                                     <div class="carousel-item">
+                                         <img src="{{asset('storage/imagens/' . $imagem[$i]->imagem)}}" class="d-block w-100"  alt="Primeiro Slide">
+                                     </div>
+                                 @endif
+                             @endfor
+                         @else
+                             <div class="carousel-item active">
+                                 <img src="{{asset('slides/aviso_5.jpg')}}" class="d-block w-100" alt="Terceiro Slide" >
+                             </div>
+                         @endif
+                     </div>
+                 </div>
+            </div>
+            <!--x Slideshow x-->
+
+            <!-- Botao proximo -->
+            <div class="col">
+              <img class="carousel-control-next" id="carrousel_botao_proximo" href="#carouselExampleControls" role="button" data-slide="next" src="{{asset('icones/proximo_azul_icone.svg')}}" alt="">
+              <!-- <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style="right: -60px">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Próximo</span>
+              </a> -->
+            </div>
+            <!--x Botao proximo x-->
+          </div>
+
         </div>
     </div>
 
@@ -62,7 +82,7 @@
     {{-- É Gratuito --}}
     <div class="row layout_index d-flex justify-content-between">
         <div class="col-sm-6">
-            <h1 class="title">É Gratuito!</h1>
+            <h1 class="title">É gratuito!</h1>
             <p class="paragraph">
                 O Cadastro é completamente <strong>gratuito</strong> e passa
                 por uma rápida avaliação, em que são verificados
@@ -71,15 +91,15 @@
             </p>
         </div>
 
-        <div class="col-sm-4">
-            <img class="image" src="{{asset('icones/festa_icone.svg')}}" alt="">
+        <div class="col-sm-6">
+            <img class="image" id="img-gratuito" src="{{asset('icones/gratuito_icone.svg')}}" alt="">
         </div>
     </div>
 
     {{-- Aumente suas vendas --}}
     <div class="row layout_index d-flex justify-content-between">
-        <div class="col-sm-4">
-            <img id="img-left" class="image" src="{{asset('icones/grafico_icone.svg')}}" alt="">
+        <div class="col-sm-6">
+            <img id="img-aumenteSuasVendas" class="image" src="{{asset('icones/grafico_icone.svg')}}" alt="">
         </div>
         <div class="col-sm-6">
             <h1 class="title">Aumente suas vendas ou prestação de serviço</h1>
@@ -92,7 +112,7 @@
         </div>
 
         <div id="img-right" class="col-sm-4">
-            <img class="image" src="{{asset('icones/grafico_icone.svg')}}" alt="">
+            <img class="image" id="img-aumenteSuasVendas2" src="{{asset('icones/grafico_icone.svg')}}" alt="">
         </div>
     </div>
 
@@ -113,15 +133,15 @@
             </div>
         </div>
 
-        <div class="col-sm-4">
-            <img class="image" src="{{asset('icones/festa_icone.svg')}}" alt="">
+        <div class="col-sm-6">
+            <img class="image" id="img_categoria" src="{{asset('icones/categorias_icone.png')}}" alt="">
         </div>
     </div>
 
     {{-- Que tal cadastrar o seu negócio? --}}
-    <div class="row layout_index d-flex justify-content-between">
+    <div class="row layout_index d-flex justify-content-between" style="padding-bottom:5rem;">
         <div class="col-sm-4">
-            <img id="img-left" class="image" src="{{asset('icones/cadastrar_negocio_icone.svg')}}" alt="">
+            <img id="img-cadastrarLeft" class="image" src="{{asset('icones/cadastrar_negocio_icone.svg')}}" alt="">
         </div>
         <div class="col-sm-6">
             <h1 class="title">Que tal cadastrar o seu negócio?</h1>
@@ -139,50 +159,9 @@
         </div>
 
         <div id="img-right" class="col-sm-4">
-            <img class="image" src="{{asset('icones/cadastrar_negocio_icone.svg')}}" alt="">
+            <img class="image" id="img_cadastrar" src="{{asset('icones/cadastrar_negocio_icone.svg')}}" alt="">
         </div>
 
-    </div>
-
-    {{-- Contato --}}
-    <div class="row layout_index d-flex justify-content-between">
-        <div class="col-sm-6">
-            <h1 class="title">Contato</h1>
-            <p class="paragraph">
-                Se você tiver alguma dúvida e/ou sugestão
-                é só enviar uma mensagem.
-            </p>
-            <img class="image" src="{{asset('icones/rede_social_icone_.svg')}}" alt="">
-        </div>
-
-        <div class="col-sm-6">
-            <div id="card-contato" class="card">
-                <div class="card-body">
-                    <form action="">
-                        @csrf
-
-                        <div class="row justify-content-center">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <label for="email">E-mail</label>
-                                    <input type="text" name="email" class="form-control">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row justify-content-center">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <label for="email">Menssagem</label>
-                                    <textarea name="menssagem" style="width:100%" rows="10"></textarea>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-        </div>
     </div>
 
 @endsection
