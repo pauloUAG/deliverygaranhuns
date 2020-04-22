7ef31f293614733d8bd305a8fdfb5c839c75e523
paulo <paulosousa1011@gmail.com>
2020-04-21 22:59:58 -0300

    Ajuste de html para que email nao caia como Spam

--- resources/views/mail/SendMailUser.blade.php
+++ resources/views/mail/SendMailUser.blade.php
@@ -1,4 +1,4 @@
 <h1>Cadastro Aceito!</h1>
 
-<p><strong>{{$user->name}}</strong> seu estabelecimento foi aprovado por um de nossos administradores!
+<p>{{$user->name}} seu estabelecimento foi aprovado por um de nossos administradores!
 Basta entrar no Encontre & Compre e logar!</p>
